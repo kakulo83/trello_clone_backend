d7a7281d93379868c886ec0e13c9394da6259841
Robert Carter <kakulo83@gmail.com>
2019-03-24 17:38:12 -0700

    update actionview

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,10 +1,13 @@
 FactoryBot.define do
   factory :user do
+    account
     name { "Mr Owner" }
     email { "owner@test.net" }
     role { :user }
     password { "foobar" }
     password_confirmation { "foobar" }
+
+    association
 
     trait :owner do
       role { :owner }
